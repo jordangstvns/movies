7112be86ea553484cd2ad62d197633942bb52ca4
jordangstvns <57878176+jordangstvns@users.noreply.github.com>
2020-11-29 18:21:03 -0500

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const autoCompleteConfig = {
   },
 
   async fetchData(searchTerm) {
-    const response = await axios.get("http://www.omdbapi.com/", {
+    const response = await axios.get("https://www.omdbapi.com/", {
       params: {
         apikey: "eb67e357",
         s: searchTerm,
@@ -48,7 +48,7 @@ let leftMovie;
 let rightMovie;
 
 const onMovieSelect = async (movie, summaryElement, side) => {
-  const response = await axios.get("http://www.omdbapi.com/", {
+  const response = await axios.get("https://www.omdbapi.com/", {
     params: {
       apikey: "eb67e357",
       i: movie.imdbID,
